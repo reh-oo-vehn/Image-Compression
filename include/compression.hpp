01d89ffdb80ec9a83c75f3ae31455bb5c1bdaa68
RV <105254699+rubenvalenz@users.noreply.github.com>
2024-08-27 21:57:21 -0600

    Edited Aug 27

--- include/compression.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef COMPRESSION_HPP
-#define COMPRESSION_HPP
-
-#include <opencv2/opencv.hpp>
-#include <string>
-
-bool compressImage(const std::string& inputPath, const std::string& outputPath, int quality);
-
-#endif
