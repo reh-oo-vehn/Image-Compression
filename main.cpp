9071f6c86544f7715a330803900c6e4c2a53c0fa
RV <105254699+rubenvalenz@users.noreply.github.com>
2024-09-01 16:59:37 -0400

    Edited Sep 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <opencv2/opencv.hpp>
+#include <iostream> 
+#include <filesystem>
 
 
 
 int main(int argc, char** argv) { 
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <image path>" << std::endl; 
+        return -1; 
+    }
+
+    cv::Mat image = cv::imread(argv[1]); 
+
+    if (image.empty()) { 
+        std::cout << "Error: Could not open or find the image. " << std::endl; 
+        return -1; 
+    }
+
+    std::filesystem::path iamgePath(argv[1]); 
+
     
+
 }
